41935854cba4336456d36e87f9302ee5e4819ac7
lonelonec <ayeayeaung96@gmail.com>
2024-02-23 21:33:25 +0630

    spa

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import {useState} from 'react';
+import Navbar from './components/Navbar/index';
+import PostsList from './components/PostsList/index';
+import Modal from './components/Modal/index';
+ 
+function App() {
+  let [showModal , setShowModal ] = useState(false);
+
+  let [posts,setPosts] = useState([
+    { 
+      id : 1,
+      title : 'First post'
+    },
+    { 
+      id : 2,
+      title : 'Second post'
+    },
+    { 
+      id : 3,
+      title : 'Third post'
+    }
+
+  ]);
+
+  return (
+    <>
+      <Navbar setShowModal = {setShowModal}/>
+      <PostsList posts= {posts} />
+      {/* <Modal/> */}
+       {/* modal content */}
+      {/* <Modal>
+        <h1>Zoom class is available now.</h1>
+        <p>feel free to <a href=''>join</a> here</p>
+      </Modal> */}
+      {showModal && <Modal>
+        <h1>This is testing</h1>
+        <p>lorem15
+          hfkgslvhew;bewhg;weohiegrgie
+        </p>
+        <button onClick={()=> setShowModal(false)}>close</button>
+      </Modal>}
+
+      
+    </>
+   
+ 
+  );
+}
+
+
+
+export default App;
+
+
+
+
+
+ 
---
+++ src/components/Modal/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './index.css';
+import ReactDom from 'react-dom';
+
+export default function Modal({children}) {
+  return (
+    ReactDom.createPortal(
+      <div className='modal-component'>
+         <div className='modal-backdrop'>
+          <div className='modal'>
+          {children}
+          </div>
+          </div>
+      </div>
+    ,document.getElementById('modal')
+    )
+  )
+}
